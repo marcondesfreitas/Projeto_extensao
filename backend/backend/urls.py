6718bc4f8d32c74abb8837b830d51858e22fc9d9
Marcondes <marcondesdeev@gmail.com>
2026-09-07 04:12:18 -0300

    correcao das urls e imagens

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("posts/", include("posts.urls")),
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
 ]
-
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
